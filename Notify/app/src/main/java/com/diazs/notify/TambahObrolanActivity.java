60f249e9a252e1e704c70bb39fd650e6388ac756
fidzfadillah <fishfadillah14@gmail.com>
2020-10-01 18:01:21 +0700

    tampilan list card di elearn

--- Notify/app/src/main/java/com/diazs/notify/TambahObrolanActivity.java
+++ Notify/app/src/main/java/com/diazs/notify/TambahObrolanActivity.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import java.util.List;
+
 public class TambahObrolanActivity extends BottomSheetDialogFragment {
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
